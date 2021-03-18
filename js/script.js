5cfd573c6e2f18898b817104cd2ebe1578c3ade4
Nicoletta Berrettoni <nico.berrettoni@gmail.com>
2021-03-18 15:58:48 +0100

    create variabili, logica applicazione e output finale - esercizio terminato

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+//dati variabili
+
+alert('Ciao! Benvenuto su questo sito!')
+
+var nomeUtente = prompt( 'Dimmi il tuo nome');
+//console.log(nomeUtente);
+
+var cognomeUtente = prompt( 'Dimmi il tuo cognome');
+//console.log(cognomeUtente);
+
+var colorePreferito = prompt( 'Qual è il tuo colore preferito?');
+//console.log(colorePreferito);
+
+//logica applicazione
+
+var passwordGenerata = 'La tua password è... ' + nomeUtente + cognomeUtente + colorePreferito + 21;
+
+//output
+
+alert(passwordGenerata);
